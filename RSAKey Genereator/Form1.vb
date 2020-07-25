6f5bf2b5842c2744d259dcbd1281578e16c96681
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:16:23 -0300

    old project migrated

---
+++ RSAKey Genereator/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim RSA As New RSACryptoServiceProvider(1024)
+
+        Dim PrivateKeyContent As String = RSA.ToXmlString(includePrivateParameters:=True)
+        Dim PublicKeyContent As String = RSA.ToXmlString(includePrivateParameters:=False)
+
+        Clipboard.SetText(PrivateKeyContent & vbCrLf & vbCrLf & PublicKeyContent)
+        MsgBox("Keys sent to clipboard", MsgBoxStyle.Information)
+        Me.Close()
+    End Sub
+
+End Class
